238c803149aef5f6c69c8ea3b4520a9b1cc9cfad
hurairaFurqan <hurairafurqan13@gmail.com>
2024-07-15 11:01:47 +1000

    middleware

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+    const path = req.nextUrl.pathname;
+
+
+    const isPublicPath = path === '/login' || path === "/signup" || path === '/verifyemail'
+
+    const token = req.cookies.get("token")?.value || '';
+    if (isPublicPath && token) {
+        console.log("you are hitting public route while having token in cookies");
+        
+        return NextResponse.redirect(new URL("/profile", req.nextUrl));
+    }
+    if (!isPublicPath && !token) {
+        console.log("you are hitting private route while not having token in cookies");
+
+        return NextResponse.redirect(new URL("/login", req.nextUrl))
+    }
+}
+
+
+export const config = {
+    matcher: ['/','/login','/signup', '/verifyemail', '/profile'],
+}
